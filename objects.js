bba87b0907e7e458fb7491fccdb73daac4976ae5
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-30 06:26:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
 var playlist = new Object({artistName: "songTitle"});
-
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi") {
-  return playlist[artistName] = "songTitle"
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist['Phil Ochs'] = "Here's to the State of Mississippi"
 }
